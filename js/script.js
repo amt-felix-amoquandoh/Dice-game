9bd94266b8ba3ccee4f5f6f2015061b4b211d51c
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-09-30 17:40:51 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ var scores, activeScore, playerActive, dice;
 
 scores = [0,0];
 activeScore = 0;
-playerActive = 0;
+playerActive = 1;
 
 dice = Math.floor(Math.random() * 6) + 1;
 
 
-document.querySelector('#current-' + playerActive).innerHTML = dice;
+document.querySelector("#current-" + playerActive).textContent = dice;
