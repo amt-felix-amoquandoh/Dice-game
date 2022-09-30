81230fa97f52b7687c89b9a4db6642c5c6806d09
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-09-30 18:40:55 +0000

    images update

--- js/script.js
+++ js/script.js
@@ -4,11 +4,17 @@ scores = [0,0];
 activeScore = 0;
 playerActive = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
 
 
-document.querySelector("#current-" + playerActive).textContent = dice;
-document.querySelector(".dice").classList.add("diceDisplay");
+// document.querySelector("#current-" + playerActive).textContent = dice;
+
+// document.querySelector(".dice").classList.add("diceDisplay");
+
 document.querySelector(".btnRoll").addEventListener("click", function(){
-    
+   // getting a random number
+    let dice = Math.floor(Math.random() * 6) + 1;
+   //displaying to UI
+   let rolledDice = document.querySelector(".dice");
+   rolledDice.classList.add("rolledDice");
+   rolledDice.src = "dice-" + dice + ".png"
 });
