4b505e389d2f7cc14d21fa3d8ca877ebf5b6188f
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-10-11 12:21:24 +0000

    script and styles update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
+let p1CurrentScore = document.getElementById("current-1");
+let p2CurrentScore = document.getElementById("current-2");
+let p1MainScore = document.getElementById("player1Score");
+let p2MianScore = document.getElementById("player2Score");
+
+
 var scores, activeScore, playerActive, dice;
 
 scores = [0,0];
 activeScore = 0;
 playerActive = 1;
 
-document.getElementById("player1Score").textContent = "0";
-document.getElementById("current-1").textContent = "0";
-document.getElementById("player2Score").textContent = "0";
-document.getElementById("current-2").textContent = "0";
-
-//  = dice;
+p1MainScore.textContent = "0";
+p1CurrentScore.textContent = "0";
+p2MianScore.textContent = "0";
+p2CurrentScore = "0";
 
 // document.querySelector(".dice").classList.add("diceDisplay");
 
@@ -28,7 +32,9 @@ function rollDice (){
       //go to player 2
        playerActive === 1 ? playerActive = 2 : playerActive = 1;
        document.getElementById("current-1").textContent = "0";
-       document.getElementById("current-2").textContent = "0";       
+       document.getElementById("current-2").textContent = "0"; 
+       document.querySelector(".player1").classList.remove("playerActive");
+       document.querySelector(".player2").classList.add("playerActive");
    }
 }
 
