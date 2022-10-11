6ba9a2c48a6e7f5bf184a788256159d4665a7916
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-10-11 11:29:14 +0000

    game files updated

--- js/script.js
+++ js/script.js
@@ -4,20 +4,22 @@ scores = [0,0];
 activeScore = 0;
 playerActive = 1;
 
-document.getElementById("player1Score").textContent = "";
-document.getElementById("current-1").textContent = "";
-document.getElementById("player2Score").textContent = "";
-document.getElementById("current-2").textContent = "";
+document.getElementById("player1Score").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+document.getElementById("player2Score").textContent = "0";
+document.getElementById("current-2").textContent = "0";
 
 // document.querySelector("#current-" + playerActive).textContent = dice;
 
 // document.querySelector(".dice").classList.add("diceDisplay");
 
-document.querySelector(".btnRoll").addEventListener("click", function(){
+function rollDice (){
    // getting a random number
     var dice = Math.floor(Math.random() * 6) + 1;
    //displaying to UI
    var rolledDice = document.querySelector(".dice");
    rolledDice.classList.add("rolledDisplay");
    rolledDice.src = "dice-" + dice + ".png"
-});
+}
+
+document.querySelector(".btnRoll").addEventListener("click", rollDice);
